35fd9be3cf3c1e38c629662ac6bc49d203f76a90
Dhivakar Gopi <dhivakar.gopi@solitontech.com>
2025-05-19 20:32:25 +0530

    basic Wrap

--- FlaUITests/NotePadTests/FileOperationTests.cs
+++ FlaUITests/NotePadTests/FileOperationTests.cs
@@ -12,18 +12,26 @@ using FlaUI.Core.Patterns;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using NotePadTests.Models;
 using NotePadTests.Utilities;
+using NotePadTests.Wrappers;
+using System;
+
+
+// Uncomment the Thread.Sleep(1000); lines if you want to add delays between actions for better visibility or debugging purposes.
 
 namespace NotePadTests
 {
     [TestClass]
     public class FileOperationTests
     {
+        string configFilePath = "C:\\Users\\Sruthi.Subaraja\\Desktop\\Dhivakar\\TestData\\ConfigFileFolder\\ConfigFile.json";
+        FolderInfo FolderInfo;
+        WrapperClass WrapperObject;
+
         [TestMethod]
-        public void CopyContent_FromExistingFile_Paste_AndSaveNewFile()
+        public void FetchFolderInfo_FromConfigFile()
         {
             //Assign
             // Fetching Data from config file.
-            string configFilePath = "C:\\Users\\Sruthi.Subaraja\\Desktop\\Dhivakar\\TestData\\ConfigFileFolder\\ConfigFile.json";
             FileHandler.LogTestData("Errors Occured:");
             if (!File.Exists(configFilePath))
             {
@@ -31,123 +39,94 @@ namespace NotePadTests
                 Assert.Fail("configFile does not exist");
             }
 
-            FolderInfo folderInfo = FileHandler.FetchConfigFileData(configFilePath);
-            if (folderInfo == null)
+            FolderInfo = FileHandler.FetchConfigFileData(configFilePath);
+            if (FolderInfo == null)
             {
                 FileHandler.LogTestData("Config File data is not in proper JSON format");
                 Assert.Fail("Config File data is not in proper JSON format");
             }
 
-            if (!File.Exists(folderInfo.SourceFilePath))
+            if (!File.Exists(FolderInfo.SourceFilePath))
             {
                 FileHandler.LogTestData("Check whether the source file exists or the folder path in config is correct");
                 Assert.Fail("Check whether the source file exists or the folder path in config is correct");
             }
+        }
 
-            if (File.Exists(folderInfo.DestinationFilePath))
+
+        [TestMethod]
+        public void CopyContent_FromExistingFile_Paste_AndSaveNewFile()
+        {
+
+            if (File.Exists(FolderInfo.DestinationFilePath))
             {
-                File.Delete(folderInfo.DestinationFilePath);
+                File.Delete(FolderInfo.DestinationFilePath);
             }
 
             //Act
             // Launching the Notepad application.
-            Application application = Application.Launch(@"notepad.exe");
-            Thread.Sleep(1000);
-            UIA3Automation automation = new UIA3Automation();
-            Window mainWindow = application.GetMainWindow(automation);
-            ConditionFactory cf = new ConditionFactory(new UIA3PropertyLibrary());
-
-            // Opening the file using the File menu.
-            MenuItem fileMenu = mainWindow.FindFirstDescendant(cf.ByName("File")).AsMenuItem();
-            fileMenu.Click();
-            Thread.Sleep(1000);
-            MenuItem openMenuItem = mainWindow.FindFirstDescendant(cf.ByName("Open...")).AsMenuItem();
-            openMenuItem.Click();
-            Thread.Sleep(1000);
-
-            //open file dialog is opened and we need to enter the file name in the text box.
-            Window openFileWindow = mainWindow.ModalWindows.FirstOrDefault(w => w.Title.Contains("Open"));
-            if (openFileWindow != null)
+            using (Application application = Application.Launch(@"notepad.exe"))
             {
-                TextBox fileNameTextBox = openFileWindow.FindFirstDescendant(cf.ByControlType(ControlType.Edit).And(cf.ByName("File name:"))).AsTextBox();
-                fileNameTextBox.Enter(folderInfo.SourceFilePath);
-                Thread.Sleep(1000);
+                WrapperObject = new WrapperClass(@"notepad.exe", new UIA3Automation());
+
+                // Opening the file using the File menu.
+                MenuItem fileMenu = WrapperObject.GetElementByName<MenuItem>("File");
+                fileMenu.Click();
+                MenuItem openMenuItem = WrapperObject.GetElementByName<MenuItem>("Open...");
+                openMenuItem.Click();
+
+                //open file dialog is opened and we need to enter the file name in the text box.
+                Window openFileWindow = WrapperObject.GetElementByName<Window>("Open");
+
+                TextBox fileNameTextBox = WrapperObject.GetElementByNameFromWindow<TextBox>(openFileWindow, "File name:");
+                fileNameTextBox.Enter(FolderInfo.SourceFilePath);
                 Keyboard.Type(VirtualKeyShort.ENTER);
-                Thread.Sleep(1000);
+
+                // Wait for the file to open and focus on the document area.
+                WrapperObject.GetElementByName<MenuItem>("Open...");
+                TextBox document = WrapperObject.GetElementByName<TextBox>("Text editor"); ;
+                document.Focus();
+
+                // Copying all contents
+                WrapperObject.CopyContentToClipBoard();
+
+                // Getting the text from the document range to compare later.
+                string ExpectedFiledata = WrapperObject.GetValueFromTextBox(document);
+
+                // Creating a new file and pasting the copied content.
+                fileMenu.Click();
+                MenuItem newMenuItem = WrapperObject.GetElementByName<MenuItem>("New");
+                newMenuItem.Click();
+
+                document.Focus();
+                WrapperObject.PasteContentFromClipBoard();
+
+                // Getting the text from the new document range to compare.
+                string ActualNewFileData = WrapperObject.GetValueFromTextBox(document);
+                Assert.AreEqual(ExpectedFiledata, ActualNewFileData);
+
+                Keyboard.TypeSimultaneously(VirtualKeyShort.CONTROL, VirtualKeyShort.KEY_S);
+
+                // Accessing the Save As window to enter the destination file name.
+                Window saveAsWindow = WrapperObject.GetElementByName<Window>("Save As");
+                fileNameTextBox = WrapperObject.GetElementByNameFromWindow<TextBox>(openFileWindow, "File name:");
+                fileNameTextBox.Enter(FolderInfo.DestinationFilePath);
+                Keyboard.Type(VirtualKeyShort.ENTER);
             }
-            else
-            {
-                FileHandler.LogTestData("Open window not found.");
-                Assert.Fail("Open window not found.");
-            }
-
-            // Wait for the file to open and focus on the document area.
-            mainWindow.FindFirstDescendant(cf.ByControlType(ControlType.Document)).Focus();
-            // Copying all contents
-            Keyboard.TypeSimultaneously(VirtualKeyShort.CONTROL, VirtualKeyShort.KEY_A);
-            Thread.Sleep(1000);
-            Keyboard.TypeSimultaneously(VirtualKeyShort.CONTROL, VirtualKeyShort.KEY_C);
-            Thread.Sleep(1000);
-
-            // Getting the text from the document range to compare later.
-            ITextPattern textPattern = mainWindow.FindFirstDescendant(cf.ByControlType(ControlType.Document)).Patterns.Text.PatternOrDefault;
-            string ExpectedFiledata = textPattern.DocumentRange.GetText(-1);
-
-            // Creating a new file and pasting the copied content.
-            fileMenu = mainWindow.FindFirstDescendant(cf.ByName("File")).AsMenuItem();
-            fileMenu.Click();
-            Thread.Sleep(1000);
-            MenuItem newMenuItem = mainWindow.FindFirstDescendant(cf.ByName("New")).AsMenuItem();
-            newMenuItem.Click();
-            Thread.Sleep(1000);
-
-            mainWindow.FindFirstDescendant(cf.ByControlType(ControlType.Document)).Focus();
-            Thread.Sleep(1000);
-            Keyboard.TypeSimultaneously(VirtualKeyShort.CONTROL, VirtualKeyShort.KEY_V);
-            Thread.Sleep(1000);
-
-            // Getting the text from the new document range to compare.
-            textPattern = mainWindow.FindFirstDescendant(cf.ByControlType(ControlType.Document)).Patterns.Text.PatternOrDefault;
-            string ActualNewFileData = textPattern.DocumentRange.GetText(-1);
-            Assert.AreEqual(ExpectedFiledata, ActualNewFileData);
-
-            Keyboard.TypeSimultaneously(VirtualKeyShort.CONTROL, VirtualKeyShort.KEY_S);
-            Thread.Sleep(1000);
-
-            // Accessing the Save As window to enter the destination file name.
-            Window saveAsWindow = mainWindow.ModalWindows.FirstOrDefault(w => w.Title.Contains("Save As"));
-            if (saveAsWindow != null)
-            {
-                TextBox fileNameTextBox = saveAsWindow.FindFirstDescendant(cf.ByControlType(ControlType.Edit).And(cf.ByName("File name:"))).AsTextBox();
-                fileNameTextBox.Enter(folderInfo.DestinationFilePath);
-                Thread.Sleep(1000);
-                Button saveButton = saveAsWindow.FindFirstDescendant(cf.ByControlType(ControlType.Button).And(cf.ByName("Save"))).AsButton();
-                saveButton.Click();
-                Thread.Sleep(1000);
-            }
-            else
-            {
-                FileHandler.LogTestData("Save As window not found.");
-                Assert.Fail("Save As window not found.");
-            }
-
-            mainWindow.Close();
-            Thread.Sleep(2000);
 
             //Assert
-            if (!File.Exists(folderInfo.DestinationFilePath))
+            if (!File.Exists(FolderInfo.DestinationFilePath))
             {
                 FileHandler.LogTestData("Destination file not created.");
                 Assert.Fail("Destination file not created.");
             }
 
-            if(!File.ReadAllText(folderInfo.SourceFilePath).Equals(File.ReadAllText(folderInfo.DestinationFilePath)))
+            if (!File.ReadAllText(FolderInfo.SourceFilePath).Equals(File.ReadAllText(FolderInfo.DestinationFilePath)))
             {
                 FileHandler.LogTestData("Destination file data is not same as the Source file Data.");
                 Assert.Fail("Destination file data is not same as the Source file Data.");
             }
         }
-
     }
 }
 
---
+++ FlaUITests/NotePadTests/WrapperClass/WrapperClass.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FlaUI.Core;
+using FlaUI.Core.AutomationElements;
+using FlaUI.Core.Conditions;
+using FlaUI.Core.Definitions;
+using FlaUI.Core.Input;
+using FlaUI.Core.Patterns;
+using FlaUI.Core.WindowsAPI;
+using FlaUI.UIA2;
+using FlaUI.UIA3;
+
+namespace NotePadTests.Wrappers
+{
+    public class WrapperClass : IDisposable
+    {
+        public Application Application { get; set; }
+        public UIA3Automation UIA3Automation { get; set; }
+        public UIA2Automation UIA2Automation { get; set; }
+        public Window MainWindow { get; set; }
+        public ConditionFactory AutomationConditionFactory { get; set; }
+
+        public WrapperClass(string executable, UIA3Automation automation)
+        {
+            Application = Application.Launch(@"notepad.exe");
+            UIA3Automation = automation;
+            AutomationConditionFactory = UIA3Automation.ConditionFactory;
+        }
+
+        public WrapperClass(string executable, UIA2Automation automation)
+        {
+            Application = Application.Launch(@"notepad.exe");
+            UIA2Automation = automation;
+            AutomationConditionFactory = UIA3Automation.ConditionFactory;
+        }
+
+        public void CopyContentToClipBoard()
+        {
+            Keyboard.TypeSimultaneously(VirtualKeyShort.CONTROL, VirtualKeyShort.KEY_A);
+            Keyboard.TypeSimultaneously(VirtualKeyShort.CONTROL, VirtualKeyShort.KEY_C);
+        }
+
+        public void PasteContentFromClipBoard()
+        {
+            Keyboard.TypeSimultaneously(VirtualKeyShort.CONTROL, VirtualKeyShort.KEY_V);
+        }
+
+        public T GetElementByName<T>(string automationElementName) where T : AutomationElement
+        {
+            return MainWindow.FindFirstDescendant(AutomationConditionFactory.ByName(automationElementName)) as T;
+        }
+        public string GetValueFromTextBox(TextBox textBox)
+        {
+            ITextPattern textPattern = textBox.Patterns.Text.PatternOrDefault;
+            return textPattern.DocumentRange.GetText(-1);
+        }
+
+        public void SetFocus(string name, ControlType controlType) { }
+
+        public void Dispose()
+        {
+            Application.Close();
+            Application.Dispose();
+        }
+
+        public T GetElementByNameFromWindow<T>(Window modalWindow, string automationElementName) where T : AutomationElement
+        {
+            return modalWindow.FindFirstDescendant(AutomationConditionFactory.ByName(automationElementName)) as T;
+        }
+    }
+}
